3ee6dad27e7c3d97f79f900156fa31ef5163d4ca
Fabian Pietsch <fabian@canvon.de>
2016-09-14 22:14:42 +0200

    [lib] Enter assembly info

--- CvnNote/Properties/AssemblyInfo.cs
+++ CvnNote/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.CompilerServices;
 // Change them to the values specific to your project.
 
 [assembly: AssemblyTitle("CvnNote")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Library for cvnnote-sharp, canvon notes in C#/.NET")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("Fabian Pietsch")]
+[assembly: AssemblyProduct("cvnnote-sharp")]
+[assembly: AssemblyCopyright("Copyright © 2016  Fabian Pietsch <fabian@canvon.de>")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -17,7 +17,7 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.*")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
