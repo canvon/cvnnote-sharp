7f1d03c15b18ea66981b4a946b717fd56180f3e8
Fabian Pietsch <fabian@canvon.de>
2016-09-15 20:53:12 +0200

    [lib] Notes: Add skeleton implementation, using nested classes

--- CvnNote/Notes.cs
+++ CvnNote/Notes.cs
@@ -1,12 +1,95 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CvnNote
 {
 	public class Notes
 	{
-		
-		public Notes()
+		public class Day
 		{
+			public class Intro
+			{
+				// TODO: Store as DateTime
+				public string Date {
+					get;
+					private set;
+				}
+
+
+				public Intro(IList<string> lines)
+				{
+					if (object.ReferenceEquals(lines, null))
+						throw new ArgumentNullException("lines");
+
+					if (lines.Count != 1)
+						throw new ArgumentException("cvnnote day intro has to be a single line", "lines");
+
+					// TODO: Parse into several fields
+					// TODO: Convert to DateTime
+					Date = lines[0];
+				}
+			}
+
+
+			public class Entry
+			{
+				public string TypeInformation {
+					get;
+					private set;
+				}
+
+
+				public Entry(IList<string> lines)
+				{
+					if (object.ReferenceEquals(lines, null))
+						throw new ArgumentNullException("lines");
+
+					if (lines.Count < 1)
+						throw new ArgumentException("cvnnote entry has to start with type information", "lines");
+
+					TypeInformation = lines[0];
+					// TODO: Process rest of data somehow.
+				}
+			}
+
+
+			public Intro DayIntro {
+				get;
+				private set;
+			}
+
+			public IList<Entry> DayEntries {
+				get;
+				private set;
+			}
+
+
+			public Day(Intro intro, IList<Entry> entries)
+			{
+				if (object.ReferenceEquals(intro, null))
+					throw new ArgumentNullException("intro");
+
+				if (object.ReferenceEquals(entries, null))
+					throw new ArgumentNullException("entries");
+
+				DayIntro = intro;
+				DayEntries = entries;
+			}
+		}
+
+
+		public IList<Day> Days {
+			get;
+			private set;
+		}
+
+
+		public Notes(IList<Day> days)
+		{
+			if (object.ReferenceEquals(days, null))
+				throw new ArgumentNullException("days");
+
+			Days = days;
 		}
 	}
 }
